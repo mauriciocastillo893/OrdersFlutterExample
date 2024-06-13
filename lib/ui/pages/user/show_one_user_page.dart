df41481b41dd7a377ede20ec0c24faea579e1ce3
CassDeLaRy <mauriciomtz893@gmail.com>
2024-06-13 09:15:25 -0600

    [FIX] Trying to show an image but failed

--- lib/ui/pages/user/show_one_user_page.dart
+++ lib/ui/pages/user/show_one_user_page.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:langspeak/config/providers/user_bloc/user_bloc.dart';
@@ -28,11 +29,33 @@ class ShowOneUserPage extends StatefulWidget {
 class _ShowOneUserPageState extends State<ShowOneUserPage> {
   File image = File('');
   TextEditingController messageController = TextEditingController();
+  final ScrollController _scrollController = ScrollController();
 
   @override
   void initState() {
-    BlocProvider.of<UserBloc>(context).add(GetMessagesEvent(widget.id));
     super.initState();
+    BlocProvider.of<UserBloc>(context).add(GetMessagesEvent(widget.id));
+  }
+
+  void _scrollToBottom() {
+    if (_scrollController.hasClients) {
+      _scrollController.jumpTo(_scrollController.position.maxScrollExtent);
+    }
+  }
+
+  Future<String?> _uploadImage(File image) async {
+    try {
+      final storageRef = FirebaseStorage.instance
+          .ref()
+          .child('chat_images/${DateTime.now().millisecondsSinceEpoch}.jpg');
+      final uploadTask = storageRef.putFile(image);
+      final snapshot = await uploadTask.whenComplete(() {});
+      final downloadUrl = await snapshot.ref.getDownloadURL();
+      return downloadUrl;
+    } catch (e) {
+      print('Error uploading image: $e');
+      return null;
+    }
   }
 
   @override
@@ -55,12 +78,19 @@ class _ShowOneUserPageState extends State<ShowOneUserPage> {
                 child: BlocBuilder<UserBloc, UserState>(
                   builder: (context, state) {
                     if (state is UserInitialState) {
+                      print('UserInitialState');
                       return const Center(
-                          child:
-                              Text("Bienvenido, estamos cargando los datos"));
+                        child: Text("Bienvenido, estamos cargando los datos"),
+                      );
                     } else if (state is UserLoadingState) {
+                      print('UserLoadingState');
                       return const Center(child: CircularProgressIndicator());
                     } else if (state is GetMessagesState) {
+                      print(
+                          'GetMessagesState: ${state.messages.length} messages loaded');
+                      WidgetsBinding.instance.addPostFrameCallback((_) {
+                        _scrollToBottom();
+                      });
                       if (state.messages.isEmpty) {
                         return const Center(
                           child: ListTile(
@@ -69,19 +99,47 @@ class _ShowOneUserPageState extends State<ShowOneUserPage> {
                         );
                       } else {
                         return ListView.builder(
+                          controller: _scrollController,
                           padding: const EdgeInsets.symmetric(horizontal: 5),
                           itemCount: state.messages.length,
                           itemBuilder: (context, index) {
                             final message = state.messages[index];
-                            return ListTile(
-                              title: Text(message.text.isEmpty
-                                  ? 'Media file'
-                                  : message.text),
+                            final isCurrentUser = message.whoSend ==
+                                FirebaseAuth.instance.currentUser!.uid;
+                            return Align(
+                              alignment: isCurrentUser
+                                  ? Alignment.centerRight
+                                  : Alignment.centerLeft,
+                              child: Container(
+                                padding: const EdgeInsets.all(10),
+                                margin: const EdgeInsets.symmetric(
+                                    vertical: 5, horizontal: 10),
+                                decoration: BoxDecoration(
+                                  color: isCurrentUser
+                                      ? Colors.blue
+                                      : Colors.grey[300],
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: message.urlMultimedia.isNotEmpty
+                                    ? Image.network(message.urlMultimedia)
+                                    : Text(
+                                        message.text,
+                                        style: TextStyle(
+                                          color: isCurrentUser
+                                              ? Colors.white
+                                              : Colors.black,
+                                        ),
+                                      ),
+                              ),
                             );
                           },
                         );
                       }
+                    } else if (state is UserErrorState) {
+                      print('UserErrorState: ${state.message}');
+                      return const Center(child: Text('Ha ocurrido un error'));
                     } else {
+                      print('Unknown State: $state');
                       return const Center(child: Text('Ha ocurrido un error'));
                     }
                   },
@@ -137,39 +195,31 @@ class _ShowOneUserPageState extends State<ShowOneUserPage> {
                       onPressed: () async {
                         final messageText = messageController.text;
                         final imagePath = image.path;
+
                         if (imagePath.isNotEmpty || messageText.isNotEmpty) {
+                          String imageUrl = '';
+                          if (imagePath.isNotEmpty) {
+                            imageUrl = (await _uploadImage(image)) ?? '';
+                          }
+
                           final message = Message(
                             whoSend: FirebaseAuth.instance.currentUser!.uid,
                             whoReceive: widget.id,
                             text: messageText,
-                            urlMultimedia: imagePath,
+                            urlMultimedia: imageUrl,
                             timestamp: Timestamp.now(),
                           );
 
                           BlocProvider.of<UserBloc>(context)
                               .add(SendMessageEvent(message, widget.id));
 
-                          if (imagePath.isNotEmpty) {
-                            final bool result = await sendImage(image);
-                            if (result) {
-                              setState(() {
-                                image = File('');
-                              });
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                  content: Text('Imagen enviada'),
-                                ),
-                              );
-                            } else {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                  content: Text('Error al enviar la imagen'),
-                                ),
-                              );
-                            }
-                          }
-
                           messageController.clear();
+                          setState(() {
+                            image = File('');
+                          });
+
+                          BlocProvider.of<UserBloc>(context).add(
+                              GetMessagesEvent(widget.id)); // Refetch messages
                         }
                       },
                     ),
